c0299e5c7f4d433513f26770697757942f74ccb6
Tolstova Yulia <yu.tolstova@minervasoft.ru>
2024-12-10 16:11:34 +0400

    добавлен попап

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header/Header';
+import {ConnectPopup} from "./components/ConnectPopup/ConnectPopup";
 
 const CN = 'jt-app';
 
 function App() {
+    const [connectPopupOpen, setConnectPopupOpen] = useState(false);
+
+
     return (
         <div className={CN}>
-            <Header />
+            <Header onOpenPopup={setConnectPopupOpen}/>
+            {connectPopupOpen &&
+                <ConnectPopup connectPopupOpen={connectPopupOpen} setConnectPopupOpen={setConnectPopupOpen}/>}
         </div>
     );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,43 +2,50 @@ import './Header.scss';
 
 const CN = 'jt-header';
 
-const Header = () => {
+const Header = ({onOpenPopup}) => {
     return (
         <header className={CN}>
-            <a>Обо мне</a>
-            <a>Опыт работы</a>
-            <a>Портфолио</a>
-            <a>Контакты</a>
+            <nav>
+                <a>Обо мне</a>
+                <a>Опыт работы</a>
+                <a>Портфолио</a>
+                <a>Контакты</a>
+            </nav>
 
-            <button className={`${CN}__btn`}>Связаться</button>
+            <button className={`${CN}__btn`} onClick={() => onOpenPopup(true)}>Связаться
+            </button>
 
-            <a
-                href={'https://t.me/iuliiatolstova'}
-                className={`${CN}__icon telegram`}
-                target="_blank"
-                rel="noopener noreferrer"
-            ></a>
-            <a
-                href={'https://www.instagram.com/_yulia_tolstova_/'}
-                className={`${CN}__icon instagram`}
-                target="_blank"
-                rel="noopener noreferrer"
-            ></a>
-            <a
-                href={'https://www.instagram.com/_yulia_tolstova_/'}
-                className={`${CN}__icon whatsapp`}
-                target="_blank"
-                rel="noopener noreferrer"
-            ></a>
-            <a
-                href={'https://www.instagram.com/_yulia_tolstova_/'}
-                className={`${CN}__icon linkedin`}
-                target="_blank"
-                rel="noopener noreferrer"
-            ></a>
+            {/*<a*/}
+            {/*    href={'https://t.me/iuliiatolstova'}*/}
+            {/*    className={`${CN}__icon telegram`}*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*></a>*/}
+            {/*<a*/}
+            {/*    href={'https://www.instagram.com/_yulia_tolstova_/'}*/}
+            {/*    className={`${CN}__icon instagram`}*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*></a>*/}
+            {/*<a*/}
+            {/*    href={'https://www.instagram.com/_yulia_tolstova_/'}*/}
+            {/*    className={`${CN}__icon whatsapp`}*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*></a>*/}
+            {/*<a*/}
+            {/*    href={'https://www.instagram.com/_yulia_tolstova_/'}*/}
+            {/*    className={`${CN}__icon linkedin`}*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*></a>*/}
 
-            <div className={`${CN}__switch}`}>
-                <div className={`${CN}__theme theme}`}></div>
+            <div className={`${CN}__switch`}>
+                <div className={`${CN}__theme light`}></div>
+            </div>
+
+            <div className={`${CN}__lang`}>
+                <div className={`${CN}__lang ru`}></div>
             </div>
         </header>
     );
